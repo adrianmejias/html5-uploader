6a999aa7374d68c2563e09fd7f64a3feab5e7d16
Adrian <adrianmejias@gmail.com>
2015-04-25 14:23:53 -0500

    tag customization

--- html5-uploader.js
+++ html5-uploader.js
@@ -1,6 +1,6 @@
 ;
 (function($) {
-    $.fn.html5Uploader = function(options) {
+    $.fn.html5uploader = function(options) {
         var defaults = {
                 accept: [
                     'image/png',
@@ -13,6 +13,8 @@
                 progress: null,
                 preview: null,
                 complete: null,
+                debug: false,
+                history: [],
                 currentProgress: 0
             },
             settings = $.extend(defaults, options),
@@ -31,6 +33,18 @@
                 progress: 'upload' in new XMLHttpRequest
             },
             /**
+             * @url http://www.paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+             * @return void
+             */
+            consoleLog = function() {
+                if (settings.debug === true) {
+                    settings.history.push(arguments);
+                    if (this.console) {
+                        console.log(Array.prototype.slice.call(arguments));
+                    }
+                }
+            },
+            /**
              * Functionality for uploading a file.
              *
              * @param  object e
@@ -39,16 +53,17 @@
              */
             fileSelect = function(e, file) {
                 //debugger;
-                console.log('fileselect', file);
+                consoleLog('fileselect', file);
                 if (!settings.url) {
-                    console.log('no url set');
+                    consoleLog('no url set');
+                    return;
                 }
                 // check if we support formdata
                 var formData = supported.formdata === true ? new FormData() : null;
                 if (supported.formdata === true) {
-                    console.log('formdata supported');
+                    consoleLog('formdata supported');
                     if (settings.progress && settings.currentProgress != 0) {
-                        console.log('progress', 0);
+                        consoleLog('progress', 0);
                         settings.currentProgress = 0;
                         settings.progress(0);
                     }
@@ -64,27 +79,28 @@
                     // most likely done
                     xhr.onload = function(e) {
                         // set progress bar to 100%
-                        if (settings.progress && settings.currentProgress != 100) {
-                            console.log('progress', 100);
-                            settings.currentProgress = 100;
-                            settings.progress(100);
-                        }
+                        setProgress(100);
                         if (e.target.responseText) {
                             var response = JSON.parse(e.target.responseText);
                             if (settings.complete) {
-                                settings.complete(file, response);
+                                if (typeof settings.complete === 'string') {
+                                    var complete = window[settings.complete];
+                                    if (typeof complete === 'function') {
+                                        complete(file, response);
+                                    }
+                                } else {
+                                    settings.complete(file, response);
+                                }
                             }
                         }
                     };
                     // progress
                     if (settings.progress && supported.progress === true) {
-                        console.log('progress supported');
+                        consoleLog('progress supported');
                         xhr.upload.onprogress = function(e) {
                             if (e.lengthComputable) {
                                 var complete = (e.loaded / e.total * 100 | 0);
-                                settings.currentProgress = complete;
-                                console.log('progress', complete);
-                                settings.progress(complete);
+                                setProgress(complete);
                             }
                         }
                     }
@@ -94,46 +110,107 @@
                     xhr.send(formData);
                 } else {
                     if (settings.complete) {
-                        settings.complete(file);
+                        if (typeof settings.complete === 'string') {
+                            var complete = window[settings.complete];
+                            if (typeof complete === 'function') {
+                                complete(file, response);
+                            }
+                        } else {
+                            settings.complete(file, response);
+                        }
                     }
                 }
                 if (settings.preview && supported.filereader === true && $.inArray(file.type, previewMimeTypes.image) != -1) {
-                    console.log('filereader supported');
+                    consoleLog('filereader supported');
                     var reader = new FileReader();
                     reader.onload = function(e) {
-                        settings.preview(e.target.result);
+                        if (typeof settings.preview === 'string') {
+                            var preview = window[settings.preview];
+                            if (typeof preview === 'function') {
+                                preview(e.target.result);
+                            }
+                        } else {
+                            settings.preview(e.target.result);
+                        }
                     };
                     reader.readAsDataURL(file);
+                }
+            },
+            setProgress = function(complete) {
+                if (settings.progress && settings.currentProgress != complete) {
+                    consoleLog('progress', complete);
+                    settings.currentProgress = complete;
+                    if (typeof settings.progress === 'string') {
+                        var progress = window[settings.progress];
+                        if (typeof progress === 'function') {
+                            progress(complete);
+                        }
+                    } else {
+                        settings.progress(complete);
+                    }
                 }
             };
         // render functionality for each file input
         return this.each(function() {
             var $input = $(this);
             if ($input.is('input')) {
-                if ($input.data('url').length) {
+                // override settings
+                if ($input.data('url') !== 'undefined') {
                     settings.url = $input.data('url');
                 }
+                if ($input.attr('accept')) {
+                    settings.accept = $input.attr('accept').split('|');
+                } else if (settings.accept) {
+                    $input.attr('accept', settings.accept.join('|'));
+                }
+                if ($input.data('fields') !== 'undefined') {
+                    var fields = $input.data('fields');
+                    if (typeof fields === 'string') {
+                        // Check for already created object
+                        var _fields = window[fields];
+                        if (typeof _fields === 'function') {
+                            settings.fields = _fields;
+                        } else {
+                            // Check for 'key=value|key=value|key=value'
+                            var dataFields = fields.split('|');
+                            if (dataFields.length) {
+                                settings.fields = {};
+                                $.each(dataFields, function(key, dataField){
+                                    var value = dataField.split('=');
+                                    if (value.length) {
+                                        eval('settings.fields.'+value[0]+' = "'+value[1]+'";');
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+                if ($input.data('holder') !== 'undefined') {
+                    settings.holder = $input.data('holder');
+                }
+                if ($input.data('progress') !== 'undefined') {
+                    settings.progress = $input.data('progress');
+                }
+                if ($input.data('complete') !== 'undefined') {
+                    settings.complete = $input.data('complete');
+                }
+                if ($input.data('preview') !== 'undefined') {
+                    settings.preview = $input.data('preview');
+                }
+                // drag n drop support
                 if (supported.dnd && settings.holder) {
-                    console.log('dnd supported');
+                    consoleLog('dnd supported');
                     $(settings.holder).on('dragover', function(e) {
                         $(this).addClass('hover');
                         $input.addClass('disabled');
                         // reset progress bar
-                        if (settings.progress && settings.currentProgress != 0) {
-                            console.log('progress', 0);
-                            settings.currentProgress = 0;
-                            settings.progress(0);
-                        }
+                        setProgress(0);
                         return false;
                     }).on('dragend', function(e) {
                         $(this).removeClass('hover');
                         $input.removeClass('disabled');
                         // reset progress bar
-                        if (settings.progress && settings.currentProgress != 0) {
-                            console.log('progress', 0);
-                            settings.currentProgress = 0;
-                            settings.progress(0);
-                        }
+                        setProgress(0);
                         return false;
                     }).on('drop', function(e) {
                         e.preventDefault();
@@ -146,9 +223,9 @@
                         // only accept mime types we provide
                         if (file) {
                             var acceptable = $.inArray(file.type, settings.accept);
-                            console.log('file chosen', file, acceptable);
+                            consoleLog('file chosen', file, acceptable);
                             if (acceptable != -1) {
-                                console.log('acceptable mime type', file.type, settings.accept);
+                                consoleLog('acceptable mime type', file.type, settings.accept);
                                 // trigger our upload functionality
                                 fileSelect(e, file);
                             }
@@ -158,17 +235,13 @@
                 $input.change(function(e) {
                     var file = $(this).get(0).files[0];
                     // reset progress bar
-                    if (settings.progress && settings.currentProgress != 0) {
-                        console.log('progress', 0);
-                        settings.currentProgress = 0;
-                        settings.progress(0);
-                    }
+                    setProgress(0);
                     // only accept mime types we provide
                     if (file) {
                         var acceptable = $.inArray(file.type, settings.accept);
-                        console.log('file chosen', file, acceptable);
+                        consoleLog('file chosen', file, acceptable);
                         if (acceptable != -1) {
-                            console.log('acceptable mime type', file.type, settings.accept);
+                            consoleLog('acceptable mime type', file.type, settings.accept);
                             // trigger our upload functionality
                             $(this).trigger('fileselect', file);
                         }
